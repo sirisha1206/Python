a7d2512633158b4a4884a89b4f61c045e2afd61c
sirisha1206 <nagasirisha1206@gmail.com>
2018-06-16 01:40:39 -0500

    icp3

--- ICP/ICP3/icp3_2.py
+++ ICP/ICP3/icp3_2.py
@@ -6,36 +6,39 @@ url = "https://en.wikipedia.org/wiki/List_of_state_and_union_territory_capitals_
 source_code = urllib.request.urlopen(url)
 soup = BeautifulSoup(source_code, "html.parser")
 
+#To find the title of the web page
 body = soup.find('title')
 print(soup.title.text)
 
+#find all the anchor tags in html page
 links = soup.find_all('a')
 print(links);
 
-
+#iterate through the anchor tags and get the corresponding href attribute
 for link in soup.find_all('a'):
     print(link.get('href'))
 
+#To get all the tables in the html page
 all_table_data = soup.find('table')
 print(all_table_data)
 
+#to get the table with classes wikitable sortable plainrowheaders
 table_data = soup.find('table', {'class': 'wikitable sortable plainrowheaders'})
 statesList=[]
 capitals=[]
 
-
+#iterate through the tr tags in table_data
 for row in table_data.findAll("tr"):
-    cells = row.findAll('td')
-    states=row.findAll('th') #To store second column data
+    cells = row.findAll('td') #store all the values of tr in cells list
+    states = row.findAll('th') #tore all the values of th in states list
     if len(cells)==6: #Only extract table body not heading
-        statesList.append(states[0].find(text=True))
-        capitals.append(cells[1].find(text=True))
+        statesList.append(states[0].find(text=True)) #get the state name and store in statesList list
+        capitals.append(cells[1].find(text=True)) #get the capital name and store it in capitals list
 
 print(states)
 print(capitals)
 
-d = {'State': statesList,'Capital':capitals}
-df = pd.DataFrame.from_dict(d)
-df
+d = {'State': statesList,'Capital':capitals} #form a dictionary of states and capitals
+df = pd.DataFrame.from_dict(d) #prepare a dataframe from the formed dictionary
 
 print(df)
