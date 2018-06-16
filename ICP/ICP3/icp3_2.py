572a96d0b3e852a9202c8354a23c2905d9ce0bca
sirisha1206 <nagasirisha1206@gmail.com>
2018-06-16 01:29:45 -0500

    icp3

--- ICP/ICP3/icp3_2.py
+++ ICP/ICP3/icp3_2.py
@@ -1,6 +1,5 @@
 import urllib.request
 from bs4 import BeautifulSoup
-import os
 import pandas as pd
 
 url = "https://en.wikipedia.org/wiki/List_of_state_and_union_territory_capitals_in_India"
@@ -21,42 +20,22 @@ all_table_data = soup.find('table')
 print(all_table_data)
 
 table_data = soup.find('table', {'class': 'wikitable sortable plainrowheaders'})
-A=[]
-B=[]
-C=[]
-D=[]
-E=[]
-F=[]
-G=[]
-#for row in table_data.findAll("tr"):
- #   td = row.findAll('td')
-  #  A.append(td[0].find(text=True))
-   # for capitals in td:
-    #    print(capitals.text);
-    #th = row.find('th')
-    #print('State Name')
-   # print(th.text)
+statesList=[]
+capitals=[]
+
 
 for row in table_data.findAll("tr"):
     cells = row.findAll('td')
     states=row.findAll('th') #To store second column data
     if len(cells)==6: #Only extract table body not heading
-        A.append(cells[0].find(text=True))
-        B.append(states[0].find(text=True))
-        C.append(cells[1].find(text=True))
+        statesList.append(states[0].find(text=True))
+        capitals.append(cells[1].find(text=True))
 
-print(A)
-print(B)
-print(C)
+print(states)
+print(capitals)
 
-import pandas as pd
-
-d = {'Number': A, 'State': B,'Capital':C}
-df = pd.DataFrame(data=d)
+d = {'State': statesList,'Capital':capitals}
+df = pd.DataFrame.from_dict(d)
 df
 
-#df=pd.DataFrame(A,columns=['Number'])
-#df['State/UT']=B
-#df['Admin_Capital']=C
-#df
 print(df)
