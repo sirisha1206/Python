4e13a87267f746c6347ca10f63305334350f5942
sirisha1206 <nagasirisha1206@gmail.com>
2018-07-03 11:37:03 -0500

    icp8

--- ICP/ICP8/icp8_1.py
+++ ICP/ICP8/icp8_1.py
@@ -6,8 +6,9 @@ A = tf.constant([1,5,3,2],shape=[2,2])
 B = tf.constant([1,7,4,2],shape=[2,2])
 C = tf.constant([3,4,5,9],shape=[2,2])
 
-D = tf.pow(A,2)
-E = tf.add(D,B)
-F = tf.multiply(E,C)
+D = tf.pow(A,2)#power of A and save it to D
+E = tf.add(D,B)# add A * A to B and save it in E
+F = tf.multiply(E,C)#multiply E and C and save it to F
 
-print(session.run(F))
+print(session.run(F))#print the result
+session.close()
